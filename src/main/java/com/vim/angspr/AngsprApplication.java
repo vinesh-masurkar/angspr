6d35ff716a053e399e5eadcd9838d1bc6867c6fc
Vinesh Masurkar <vineshmasurkr@yahoo.com>
2019-02-27 21:46:39 -0500

    Added application.

---
+++ src/main/java/com/vim/angspr/AngsprApplication.java
@@ -0,0 +1,13 @@
+package com.vim.angspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngsprApplication {
+	// SSH: git@github.com:vineshmasurkar/angspr.git
+	public static void main(String[] args) {
+		SpringApplication.run(AngsprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vim/angspr/AngsprApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vim.angspr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AngsprApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
