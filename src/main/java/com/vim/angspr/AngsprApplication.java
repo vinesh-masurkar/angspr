c88ccc6a2f0c74545544a2dad22e151a9493e71b
Masurkar <vineshmasurkar@yahoo.com>
2019-02-28 13:15:12 -0500

    Added SSH and HTTPS git location to the application comments.

--- src/main/java/com/vim/angspr/AngsprApplication.java
+++ src/main/java/com/vim/angspr/AngsprApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AngsprApplication {
-	// SSH: git@github.com:vineshmasurkar/angspr.git
+	// SSH:   git@github.com:masurkarvinesh/angspr.git
+	// HTTPS: https://github.com/masurkarvinesh/angspr.git
 	public static void main(String[] args) {
 		SpringApplication.run(AngsprApplication.class, args);
 	}
-
 }
